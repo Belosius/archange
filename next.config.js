8410e4db282093ef9600e11a05ae2667a74e9a2e
Belosius <teissedre.olivier@gmail.com>
2026-04-13 12:44:28 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: { serverComponentsExternalPackages: ['googleapis'] },
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
 }
 module.exports = nextConfig
